5a737cf204a16bc5c898cc8a6e7d123c76d42ccd
Haris Khan <hariskhan@Hariss-Mac.local>
2024-04-27 13:14:14 +0530

    Initial commit

---
+++ I am rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I am rich
+//
+//  Created by Haris Khan on 25/04/24.
+//  Copyright © 2024 Haris Khan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
